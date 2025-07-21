70d2c9ebe820ef1d875e3cde38e5b39f3d716bdf
takiido <takiid0@proton.me>
2025-07-21 15:34:54 -0500

    Created themes support

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,112 +1,67 @@
 'use client';
 
 import Image from "next/image";
-import styles from "./page.module.css";
 import { Button } from "./components/ui/Button";
 import { Slider } from "./components/ui/Slider";
 import { Textbox } from "./components/ui/Textbox";
 
+import '../styles/globals.scss';
+import { useState, useEffect } from "react";
+import { 
+  FaAirbnb,
+  FaMoon,
+  FaSun,
+} from "react-icons/fa";
+
 export default function Home() {
-  const test = () => {
+  const [theme, setTheme] = useState<'light' | 'dark'>('light');
+
+  const test = () => {  
     console.log("test");
   }
 
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const handleThemeChange = () => {
+    setTheme(theme === 'light' ? 'dark' : 'light');
+  }
 
+  useEffect(() => {
+    const isDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+    setTheme(isDark ? 'dark' : 'light');
+  }, []);
+
+  useEffect(() => {
+    document.documentElement.setAttribute('data-theme', theme);
+  }, [theme]);
+
+  return (
+    <div>
+      <main>
+        Theme toggle
+        <Button 
+          text="Toggle theme" 
+          icon={theme === 'light' ? <FaSun /> : <FaMoon />} 
+          onClick={handleThemeChange} 
+        />
+        Default button
         <Button text="Generate" onClick={test} />
+        Disabled button
         <Button text="Generate" onClick={test} disabled />
-        <Button onClick={test} icon={<Image src="/vercel.svg" alt="Vercel logomark" width={20} height={20} />} />
-        <Button onClick={test} icon={<Image src="/vercel.svg" alt="Vercel logomark" width={20} height={20} />} disabled />
-        <Button text="Generate" onClick={test} icon={<Image src="/vercel.svg" alt="Vercel logomark" width={20} height={20} />} disabled />
+        Icon button
+        <Button onClick={test} icon={<FaAirbnb />} />
+        Disabled icon button
+        <Button onClick={test} icon={<FaAirbnb />} disabled />
+        Disabled text and icon button
+        <Button text="Generate" onClick={test} icon={<FaAirbnb />} disabled />
+        Slider
         <Slider min={1} max={6} initialValue={4} />
+        Textbox
         <Textbox placeholder="Hello" onChange={test} initialValue="Hello" />
-        
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
+        Disabled textbox
+        <Textbox placeholder="Hello" onChange={test} initialValue="Hello" disabled />
+        Readonly textbox
+        <Textbox placeholder="Hello" onChange={test} initialValue="Hello" readonly />
+
       </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
   );
 }
