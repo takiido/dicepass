5c0bee2599f153b82fab7ac1dd8b2dc5e5f08e97
takiido <takiid0@proton.me>
2025-07-31 17:03:18 -0500

    Updated meta

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,29 @@ import type { Metadata } from "next";
 export const metadata: Metadata = {
   title: "Dicepass",
   description: "Diceware password generator",
+  openGraph: {
+    title: "Dicepass",
+    description: "Diceware password generator",
+    url: "https://dicepass.takiido.dev",
+    siteName: "Dicepass",
+    images: [
+      {
+        url: "https://dicepass.takiido.dev/preview.png",
+        width: 1200,
+        height: 630,
+        alt: "Dicepass Logo",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Dicepass",
+    description: "Diceware password generator",
+    images: ["https://dicepass.takiido.dev/preview.png"],
+    site: "@eviltakiido",
+  },
 };
 
 export default function RootLayout({
